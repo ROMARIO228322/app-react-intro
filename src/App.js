38720a0ebc15a86f36c057a6ef113838735f518f
Romario <Romario@LAPTOP-1NT5U89V>
2024-11-23 19:42:20 +0200

    Hedy Lamarr avatar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-function MyButton() {
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
+  export default function Profile() {
     return (
-      <button>
-        Button
-      </button>
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
     );
   }
   
-  export default function MyApp() {
-    return (
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton />
-      </div>
-    );
-  }
